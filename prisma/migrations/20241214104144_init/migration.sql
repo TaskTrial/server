31d6a6e71e295a86d01c8715d6a7811f13234103
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-12-14 12:45:21 +0200

    refactor: update prisma structure

--- prisma/migrations/20241214104144_init/migration.sql
+++ prisma/migrations/20241214104144_init/migration.sql
@@ -1,5 +1,8 @@
 -- CreateEnum
-CREATE TYPE "UserRole" AS ENUM ('OWNER', 'MANAGER', 'ADMIN', 'MEMBER', 'GUEST');
+CREATE TYPE "ReportType" AS ENUM ('ORGANIZATION', 'DEPARTMENT', 'TEAM', 'PROJECT', 'TASK', 'USER_PERFORMANCE');
+
+-- CreateEnum
+CREATE TYPE "EntityType" AS ENUM ('ORGANIZATION', 'TEAM');
 
 -- CreateEnum
 CREATE TYPE "TaskPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
@@ -8,10 +11,13 @@ CREATE TYPE "TaskPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
 CREATE TYPE "TaskStatus" AS ENUM ('TODO', 'IN_PROGRESS', 'DONE');
 
 -- CreateEnum
+CREATE TYPE "DependencyType" AS ENUM ('BLOCKS', 'REQUIRES', 'RELATES_TO', 'DUPLICATES');
+
+-- CreateEnum
 CREATE TYPE "TeamMemberRole" AS ENUM ('MEMBER', 'LEADER', 'VIEWER');
 
 -- CreateEnum
-CREATE TYPE "ReportType" AS ENUM ('ORGANIZATION', 'DEPARTMENT', 'TEAM', 'PROJECT', 'TASK', 'USER_PERFORMANCE');
+CREATE TYPE "UserRole" AS ENUM ('OWNER', 'MANAGER', 'ADMIN', 'MEMBER', 'GUEST');
 
 -- CreateTable
 CREATE TABLE "timelogs" (
@@ -40,12 +46,13 @@ CREATE TABLE "comments" (
 -- CreateTable
 CREATE TABLE "departments" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "organizationId" UUID NOT NULL,
     "managerId" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "departments_pkey" PRIMARY KEY ("id")
 );
@@ -56,9 +63,10 @@ CREATE TABLE "notifications" (
     "userId" UUID NOT NULL,
     "content" TEXT NOT NULL,
     "isRead" BOOLEAN NOT NULL DEFAULT false,
-    "type" TEXT NOT NULL,
+    "type" VARCHAR(50) NOT NULL,
     "metadata" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
 );
@@ -66,16 +74,17 @@ CREATE TABLE "notifications" (
 -- CreateTable
 CREATE TABLE "organizations" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
-    "industry" TEXT NOT NULL,
-    "sizeRange" TEXT NOT NULL,
-    "website" TEXT,
+    "industry" VARCHAR(50) NOT NULL,
+    "sizeRange" VARCHAR(50) NOT NULL,
+    "website" VARCHAR(255),
     "logoUrl" TEXT,
     "isVerified" BOOLEAN NOT NULL DEFAULT false,
-    "status" TEXT NOT NULL,
+    "status" VARCHAR(20) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
     "createdBy" UUID NOT NULL,
 
     CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
@@ -95,9 +104,9 @@ CREATE TABLE "organization_owners" (
 -- CreateTable
 CREATE TABLE "projects" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
-    "status" TEXT NOT NULL,
+    "status" VARCHAR(20) NOT NULL,
     "createdBy" UUID NOT NULL,
     "organizationId" UUID NOT NULL,
     "teamId" UUID NOT NULL,
@@ -105,6 +114,7 @@ CREATE TABLE "projects" (
     "endDate" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
 );
@@ -113,11 +123,11 @@ CREATE TABLE "projects" (
 CREATE TABLE "sprints" (
     "id" UUID NOT NULL,
     "projectId" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "startDate" TIMESTAMP(3) NOT NULL,
     "endDate" TIMESTAMP(3) NOT NULL,
-    "status" TEXT NOT NULL,
+    "status" VARCHAR(20) NOT NULL,
 
     CONSTRAINT "sprints_pkey" PRIMARY KEY ("id")
 );
@@ -125,15 +135,15 @@ CREATE TABLE "sprints" (
 -- CreateTable
 CREATE TABLE "reports" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "reportType" "ReportType" NOT NULL,
-    "format" TEXT NOT NULL,
+    "format" VARCHAR(20) NOT NULL,
     "parameters" JSONB,
     "filePath" TEXT NOT NULL,
     "generatedBy" UUID NOT NULL,
     "entityId" UUID NOT NULL,
-    "entityType" TEXT NOT NULL,
+    "entityType" "EntityType" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
@@ -142,7 +152,7 @@ CREATE TABLE "reports" (
 -- CreateTable
 CREATE TABLE "tasks" (
     "id" UUID NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" VARCHAR(200) NOT NULL,
     "description" TEXT,
     "priority" "TaskPriority" NOT NULL,
     "status" "TaskStatus" NOT NULL,
@@ -154,6 +164,7 @@ CREATE TABLE "tasks" (
     "dueDate" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
 );
@@ -162,8 +173,8 @@ CREATE TABLE "tasks" (
 CREATE TABLE "task_attachments" (
     "id" UUID NOT NULL,
     "taskId" UUID NOT NULL,
-    "fileName" TEXT NOT NULL,
-    "fileType" TEXT NOT NULL,
+    "fileName" VARCHAR(255) NOT NULL,
+    "fileType" VARCHAR(50) NOT NULL,
     "filePath" TEXT NOT NULL,
     "fileSize" INTEGER NOT NULL,
     "uploadedBy" UUID NOT NULL,
@@ -177,7 +188,7 @@ CREATE TABLE "task_dependencies" (
     "id" UUID NOT NULL,
     "taskId" UUID NOT NULL,
     "dependentTaskId" UUID NOT NULL,
-    "dependencyType" TEXT NOT NULL,
+    "dependencyType" "DependencyType" NOT NULL,
 
     CONSTRAINT "task_dependencies_pkey" PRIMARY KEY ("id")
 );
@@ -185,13 +196,14 @@ CREATE TABLE "task_dependencies" (
 -- CreateTable
 CREATE TABLE "teams" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "createdBy" UUID NOT NULL,
     "organizationId" UUID NOT NULL,
     "departmentId" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "teams_pkey" PRIMARY KEY ("id")
 );
@@ -211,10 +223,10 @@ CREATE TABLE "team_members" (
 -- CreateTable
 CREATE TABLE "users" (
     "id" UUID NOT NULL,
-    "email" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "role" "UserRole" NOT NULL,
     "profilePic" TEXT,
     "departmentId" UUID,
@@ -223,9 +235,97 @@ CREATE TABLE "users" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "timelogs_taskId_idx" ON "timelogs"("taskId");
+
+-- CreateIndex
+CREATE INDEX "timelogs_userId_idx" ON "timelogs"("userId");
+
+-- CreateIndex
+CREATE INDEX "comments_taskId_idx" ON "comments"("taskId");
+
+-- CreateIndex
+CREATE INDEX "comments_userId_idx" ON "comments"("userId");
+
+-- CreateIndex
+CREATE INDEX "departments_organizationId_idx" ON "departments"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "departments_managerId_idx" ON "departments"("managerId");
+
+-- CreateIndex
+CREATE INDEX "notifications_userId_idx" ON "notifications"("userId");
+
+-- CreateIndex
+CREATE INDEX "organizations_createdBy_idx" ON "organizations"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "organization_owners_organizationId_idx" ON "organization_owners"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "organization_owners_userId_idx" ON "organization_owners"("userId");
+
+-- CreateIndex
+CREATE INDEX "projects_organizationId_idx" ON "projects"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "projects_teamId_idx" ON "projects"("teamId");
+
+-- CreateIndex
+CREATE INDEX "projects_createdBy_idx" ON "projects"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "sprints_projectId_idx" ON "sprints"("projectId");
+
+-- CreateIndex
+CREATE INDEX "reports_generatedBy_idx" ON "reports"("generatedBy");
+
+-- CreateIndex
+CREATE INDEX "reports_entityId_idx" ON "reports"("entityId");
+
+-- CreateIndex
+CREATE INDEX "tasks_projectId_idx" ON "tasks"("projectId");
+
+-- CreateIndex
+CREATE INDEX "tasks_sprintId_idx" ON "tasks"("sprintId");
+
+-- CreateIndex
+CREATE INDEX "tasks_createdBy_idx" ON "tasks"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "tasks_assignedTo_idx" ON "tasks"("assignedTo");
+
+-- CreateIndex
+CREATE INDEX "task_attachments_taskId_idx" ON "task_attachments"("taskId");
+
+-- CreateIndex
+CREATE INDEX "task_attachments_uploadedBy_idx" ON "task_attachments"("uploadedBy");
+
+-- CreateIndex
+CREATE INDEX "task_dependencies_taskId_idx" ON "task_dependencies"("taskId");
+
+-- CreateIndex
+CREATE INDEX "task_dependencies_dependentTaskId_idx" ON "task_dependencies"("dependentTaskId");
+
+-- CreateIndex
+CREATE INDEX "teams_organizationId_idx" ON "teams"("organizationId");
+
+-- CreateIndex
+CREATE INDEX "teams_departmentId_idx" ON "teams"("departmentId");
+
+-- CreateIndex
+CREATE INDEX "teams_createdBy_idx" ON "teams"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "team_members_teamId_idx" ON "team_members"("teamId");
+
+-- CreateIndex
+CREATE INDEX "team_members_userId_idx" ON "team_members"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
@@ -233,47 +333,53 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
--- AddForeignKey
-ALTER TABLE "timelogs" ADD CONSTRAINT "timelogs_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "users_departmentId_idx" ON "users"("departmentId");
+
+-- CreateIndex
+CREATE INDEX "users_organizationId_idx" ON "users"("organizationId");
 
 -- AddForeignKey
-ALTER TABLE "timelogs" ADD CONSTRAINT "timelogs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "timelogs" ADD CONSTRAINT "timelogs_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "timelogs" ADD CONSTRAINT "timelogs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "comments" ADD CONSTRAINT "comments_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "departments" ADD CONSTRAINT "departments_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "comments" ADD CONSTRAINT "comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "departments" ADD CONSTRAINT "departments_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "departments" ADD CONSTRAINT "departments_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "notifications" ADD CONSTRAINT "notifications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "organization_owners" ADD CONSTRAINT "organization_owners_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "organization_owners" ADD CONSTRAINT "organization_owners_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "organization_owners" ADD CONSTRAINT "organization_owners_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "organization_owners" ADD CONSTRAINT "organization_owners_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "projects" ADD CONSTRAINT "projects_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "projects" ADD CONSTRAINT "projects_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "projects" ADD CONSTRAINT "projects_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "projects" ADD CONSTRAINT "projects_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "projects" ADD CONSTRAINT "projects_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "sprints" ADD CONSTRAINT "sprints_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "sprints" ADD CONSTRAINT "sprints_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "reports" ADD CONSTRAINT "reports_generatedBy_fkey" FOREIGN KEY ("generatedBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -285,7 +391,7 @@ ALTER TABLE "reports" ADD CONSTRAINT "reports_organization_entityId_fkey" FOREIG
 ALTER TABLE "reports" ADD CONSTRAINT "reports_team_entityId_fkey" FOREIGN KEY ("entityId") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "tasks" ADD CONSTRAINT "tasks_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "tasks" ADD CONSTRAINT "tasks_sprintId_fkey" FOREIGN KEY ("sprintId") REFERENCES "sprints"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -297,31 +403,31 @@ ALTER TABLE "tasks" ADD CONSTRAINT "tasks_createdBy_fkey" FOREIGN KEY ("createdB
 ALTER TABLE "tasks" ADD CONSTRAINT "tasks_assignedTo_fkey" FOREIGN KEY ("assignedTo") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "task_attachments" ADD CONSTRAINT "task_attachments_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "task_attachments" ADD CONSTRAINT "task_attachments_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "task_attachments" ADD CONSTRAINT "task_attachments_uploadedBy_fkey" FOREIGN KEY ("uploadedBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "task_dependencies" ADD CONSTRAINT "task_dependencies_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "task_dependencies" ADD CONSTRAINT "task_dependencies_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "task_dependencies" ADD CONSTRAINT "task_dependencies_dependentTaskId_fkey" FOREIGN KEY ("dependentTaskId") REFERENCES "tasks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "task_dependencies" ADD CONSTRAINT "task_dependencies_dependentTaskId_fkey" FOREIGN KEY ("dependentTaskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "teams" ADD CONSTRAINT "teams_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "teams" ADD CONSTRAINT "teams_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "teams" ADD CONSTRAINT "teams_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "teams" ADD CONSTRAINT "teams_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "teams" ADD CONSTRAINT "teams_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "team_members" ADD CONSTRAINT "team_members_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "team_members" ADD CONSTRAINT "team_members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "team_members" ADD CONSTRAINT "team_members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "departments"("id") ON DELETE SET NULL ON UPDATE CASCADE;

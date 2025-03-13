702eb9e4681051457bdcc2cb4726ebbf5992f112
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-13 12:05:41 +0200

    chore: setup `index.js` and install `dotenv` package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+const PORT = process.env.PORT;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running in ${process.env.NODE_ENV} enviroment on port ${PORT}`);
+});

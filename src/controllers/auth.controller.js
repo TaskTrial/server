41dd6ca26f2d8ccd321002bee0472ec94dd760ea
Your Name <you@example.com>
2025-03-27 21:22:20 +0200

    fix: solve the refresh acess token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -259,6 +259,8 @@ export const resetPassword = async (req, res) => {
     return res.status(200).json({ message: 'Password reset successfully' });
   } catch (error) {
     return res.status(500).json({ message: 'Password reset failed', error });
+  }
+};
 
 export const refreshAccessToken = async (req, res) => {
   try {

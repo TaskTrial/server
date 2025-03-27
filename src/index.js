ac7f9b9457ff62e2432e9064aed7e91241361399
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-27 12:41:03 +0200

    fix: add parse application/json middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import authRouter from './routes/auth.routes.js';
 
 /* eslint no-undef: off */
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
+
+app.use(authRouter);
 
 app.listen(PORT, () => {
   /* eslint no-console:off */

90f645873e488265f673fa218209659095b67c82
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-03-27 21:13:41 +0200

    feat: add route for `resetPassword` function

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   forgotPassword,
+  resetPassword,
   signin,
   signup,
   verifyEmail,
@@ -12,5 +13,6 @@ router.post('/api/auth/signup', signup);
 router.post('/api/auth/verifyEmail', verifyEmail);
 router.post('/api/auth/signin', signin);
 router.post('/api/auth/forgotPassword', forgotPassword);
+router.post('/api/auth/resetPassword', resetPassword);
 
 export default router;

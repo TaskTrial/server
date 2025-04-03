6b1cacdbfc6d17800efd90e48f29db42f1bf5c76
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-04-03 11:33:50 +0200

    fix: error handler middleware

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -3,12 +3,11 @@ export const notFound = (req, res, next) => {
   res.status(404);
   next(error);
 };
+
 /* eslint-disable */
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack, // Hide stack trace in production
-    statusCode,
   });
 };

d7ae209e307266cd1149c85e8195fac62c17234d
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-04-05 11:15:19 +0200

    feat: add cloudinary utils

---
+++ src/utils/cloudinary.utils.js
@@ -0,0 +1,23 @@
+import cloudinary from '../config/cloudinary.js';
+
+/**
+ * Upload image to Cloudinary
+ * fileBuffer - The image file buffer from Multer
+ * folder - The folder in Cloudinary ('organization_logos' or 'profile_pictures')
+ * Return- The secure URL of the uploaded image
+ */
+export const uploadToCloudinary = (fileBuffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result.secure_url);
+        }
+      },
+    );
+    uploadStream.end(fileBuffer);
+  });
+};

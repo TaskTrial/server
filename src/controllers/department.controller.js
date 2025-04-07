6b6f2942cdfa36242058d3cdb2bd0f224af1f9c3
Your Name <you@example.com>
2025-04-07 13:41:05 +0200

    feat: add create daprtment

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -54,3 +54,96 @@ export const getAllDepartments = async (req, res, next) => {
     next(error);
   }
 };
+
+export const createDepartment = async (req, res, next) => {
+  try {
+    const { name, description, organizationId, managerId } = req.body;
+
+    // Check required fields
+    if (!name || !organizationId || !managerId) {
+      return res
+        .status(400)
+        .json({ message: 'Name, organizationId, and managerId are required' });
+    }
+
+    // Ensure department name is unique within the same organization
+    const existingDepartment = await prisma.department.findFirst({
+      where: {
+        name,
+        organizationId,
+        deletedAt: null,
+      },
+    });
+
+    if (existingDepartment) {
+      return res
+        .status(409)
+        .json({
+          message: 'Department name already exists in this organization',
+        });
+    }
+
+    const Is_Owner = await prisma.organization.findFirst({
+      where: {
+        id: organizationId,
+        ownerId: req.managerId,
+      },
+    });
+    if (!Is_Owner) {
+      return res
+        .status(403)
+        .json({
+          message:
+            'You do not have permission to create a department in this organization',
+        });
+    }
+
+    // Check if manager exists, is active, and belongs to the same organization
+    const manager = await prisma.user.findFirst({
+      where: {
+        id: managerId,
+        deletedAt: null,
+      },
+    });
+    //TODO: Check if the manager belongs to the same organization
+    if (!manager) {
+      return res
+        .status(404)
+        .json({
+          message: 'Manager not found or does not belong to this organization',
+        });
+    }
+
+    // Create the department
+    const newDepartment = await prisma.department.create({
+      data: {
+        name,
+        description,
+        organizationId,
+        managerId,
+      },
+      include: {
+        organization: { select: { id: true, name: true } },
+        manager: {
+          select: { id: true, firstName: true, lastName: true, role: true },
+        },
+      },
+    });
+
+    // Assign "MANAGER" role to the manager
+    await prisma.user.update({
+      where: { id: managerId },
+      data: {
+        departmentId: newDepartment.id,
+        role: 'MANAGER',
+      },
+    });
+
+    return res.status(201).json({
+      message: 'Department created successfully',
+      department: newDepartment,
+    });
+  } catch (error) {
+    next(error);
+  }
+};

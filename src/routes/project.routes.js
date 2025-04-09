2f87113f16d2f1d0fa7208f8ea6c6de81fad8f80
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-04-10 00:19:38 +0200

    feat: add route for create project controller

---
+++ src/routes/project.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { verifyAccessToken } from '../middlewares/auth.middleware.js';
+import { createProject } from '../controllers/project.controller.js';
+
+const router = Router();
+
+router.post(
+  '/api/organization/:organizationId/department/:departmentId/team/:teamId/project',
+  verifyAccessToken,
+  createProject,
+);
+
+export default router;

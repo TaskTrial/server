8d1c294e70bcc0c5ba4dcb10c87eb0efb2175d80
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-04-12 18:18:03 +0200

    feat: add route for `updateProject` controller

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../middlewares/auth.middleware.js';
-import { createProject } from '../controllers/project.controller.js';
+import {
+  createProject,
+  updateProject,
+} from '../controllers/project.controller.js';
 
 const router = Router();
 
@@ -8,6 +11,12 @@ router.post(
   '/api/organization/:organizationId/team/:teamId/project',
   verifyAccessToken,
   createProject,
+);
+
+router.put(
+  '/api/organization/:organizationId/team/:teamId/project/:projectId',
+  verifyAccessToken,
+  updateProject,
 );
 
 export default router;

0105a67c97a3daf3f66210c59905630effbfe9ec
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-04-17 19:17:08 +0200

    feat: add route for `updateTask` controller function

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../middlewares/auth.middleware.js';
-import { createTask } from '../controllers/task.controller.js';
+import { createTask, updateTask } from '../controllers/task.controller.js';
 
 const router = Router();
 
@@ -8,6 +8,12 @@ router.post(
   '/api/organization/:organizationId/team/:teamId/project/:projectId/task/create',
   verifyAccessToken,
   createTask,
+);
+
+router.put(
+  '/api/organization/:organizationId/team/:teamId/project/:projectId/task/:taskId',
+  verifyAccessToken,
+  updateTask,
 );
 
 export default router;

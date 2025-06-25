fdbe61d3943efaea9c510efeb1c8e3dccc165edd
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-06-25 19:09:15 +0300

    chore: e2e jest config file

---
+++ jest.e2e.config.js
@@ -0,0 +1,14 @@
+const baseConfig = {
+  bail: true,
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/e2e/**/*.e2e.test.js'],
+  verbose: true,
+  transform: {
+    '^.+\\.jsx?$': ['babel-jest', { configFile: './babel.config.json' }],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(.*(socket.io|engine.io))/)'],
+  setupFilesAfterEnv: ['./src/__tests__/setup.e2e.js'],
+  testTimeout: 60000,
+};
+
+export default baseConfig;

9899110e7dacbfba6515d079a2e196821c040a48
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2025-06-25 15:28:49 +0300

    feat: add jest integration config

---
+++ jest.integration.config.js
@@ -0,0 +1,27 @@
+export default {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+      },
+    ],
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testMatch: ['**/src/__tests__/integration/**/*.test.js'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/__tests__/setup.js',
+    '<rootDir>/src/__tests__/setup.integration.js',
+  ],
+  collectCoverage: false,
+  coverageDirectory: '<rootDir>/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
+  verbose: true,
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@babel)'],
+  rootDir: '.',
+};
